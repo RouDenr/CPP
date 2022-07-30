663d279a689f0cda3f2702517c292ae85e1f6074
decordel <decordel@student.21-school.ru>
2022-07-30 22:15:47 +0300

    fix makefile

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -7,10 +7,10 @@ int main(int argc, char const *argv[]) {
     if (argc == 1) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     } else {
-        for (size_t i = 1; i < argc; i++) {
+        for (size_t i = 1; argv[i]; i++) {
         std::string mess = argv[i];
             for (size_t j = 0; j < mess.length(); j++) {
-                std::cout << static_cast<char>(std::toupper(mess[j]));
+                std::cout << (char)(std::toupper(mess[j]));
             }
         }
     }
