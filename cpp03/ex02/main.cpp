c698a928a936e63d71a62fa08f76b451ba16fce0
decordel <decordel@student.21-school.ru>
2022-08-06 05:01:01 +0300

    add cpp03:ex02

--- cpp03/ex02/main.cpp
+++ cpp03/ex02/main.cpp
@@ -3,15 +3,20 @@
 #include <iostream>
 #include "ClapTrap.h"
 #include "ScavTrap.h"
+#include "FragTrap.h"
 
 int main(void) {
     ClapTrap wolterTrap("Wolter");
     ScavTrap tedTrap("Ted");
+    FragTrap houstonTrap("Houston");
 
+    houstonTrap.beRepaired(0);
     wolterTrap.attack(tedTrap.getName());
     tedTrap.takeDamage(wolterTrap.getAttackDamage());
     tedTrap.attack(wolterTrap.getName());
     wolterTrap.takeDamage(tedTrap.getAttackDamage());
+    wolterTrap.beRepaired(80);
+    houstonTrap.highFivesGuys();
     tedTrap.guardGate();
     return 0;
 }
