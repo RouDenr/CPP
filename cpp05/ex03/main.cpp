2eaaeb08aa8dd6c6399ad69fae30ac6d4aad10a0
decordel <decordel@student.21-school.ru>
2022-08-12 18:57:02 +0300

    fix: cpp05:ex03 add user input

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -8,26 +8,39 @@
 #include <iostream>
 
 
-int main() {
+int main(int argc, char **argv) {
     Intern intern;
     Bureaucrat bob("Bob", 1);
-    try {
-        Form* pardonForm;
-        pardonForm = intern.makeForm("presidential pardan", "Micky");
-        bob.signForm(*pardonForm);
-        delete pardonForm;
-    } catch (Intern::FormNotExist& e) {
-        std::cout << e.what() << std::endl;
-    }
-    try {
-        Form* pardonForm;
-        pardonForm = intern.makeForm("presidential pardon", "Micky");
+    if (argc == 1) {
+        try {
+            Form* pardonForm;
+            pardonForm = intern.makeForm("presidential pardan", "Micky");
+            bob.signForm(*pardonForm);
+            delete pardonForm;
+        } catch (Intern::FormNotExist& e) {
+            std::cout << e.what() << std::endl;
+        }
+        try {
+            Form* pardonForm;
+            pardonForm = intern.makeForm("presidential pardon", "Micky");
 
-        bob.signForm(*pardonForm);
-        bob.executeForm(*pardonForm);
-        delete pardonForm;
-    } catch (Intern::FormNotExist& e) {
-        std::cout << e.what() << std::endl;
+            bob.signForm(*pardonForm);
+            bob.executeForm(*pardonForm);
+            delete pardonForm;
+        } catch (Intern::FormNotExist& e) {
+            std::cout << e.what() << std::endl;
+        }
+    } else {
+        try {
+            Form* pardonForm;
+            pardonForm = intern.makeForm(argv[1], "Micky");
+
+            bob.signForm(*pardonForm);
+            bob.executeForm(*pardonForm);
+            delete pardonForm;
+        } catch (Intern::FormNotExist& e) {
+            std::cout << argv[1] << ": " << e.what() << std::endl;
+        }
     }
     return 0;
 }
